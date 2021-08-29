1995c8a65a25b09cc774b9ccf9ead6506c952a40
Alex Sapozhnikov <alexei_sap@yahoo.com>
2021-08-29 18:17:31 -0500

    Fixed a bug with service provider scope

--- Api/Controller/ApplicationController.cs
+++ Api/Controller/ApplicationController.cs
@@ -24,6 +24,7 @@ namespace Api.Controller
         public async Task<IActionResult> GetStatusAsync() =>
             Ok(await Task.FromResult(new
             {
+                Memory = $"{GC.GetTotalMemory(true) / 1000000m:#.00} MB",
                 IsProduction = _environment.IsProduction(),
                 Server = new
                 {
